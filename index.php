5df63d796fce2e06e10b3788a77bc84cfab1a62f
Trong Anh <51241421+anhtk20012@users.noreply.github.com>
2024-05-21 06:08:45 +0700

    Add file

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome to My Simple Website</title>
+</head>
+<body>
+    <h1>Success! Your Apache Virtual Host is working!</h1>
+    <p>This is a simple static HTML page.</p>
+</body>
+</html>
